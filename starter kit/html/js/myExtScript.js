a15ffd69cd8bf430d9bc72b21c5bcdf38121c839
FalconRider <pgitschner@outlook.com>
2017-07-24 09:55:43 -0400

    Add files via upload

---
+++ starter kit/html/js/myExtScript.js
@@ -0,0 +1,6 @@
+ 
+ var currentDate = new Date(),
+      day = currentDate.getDate(),
+      month = currentDate.getMonth() + 1,
+      year = currentDate.getFullYear();
+  document.write(day + "/" + month + "/" + year)
